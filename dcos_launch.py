f1640a091f7bf9fff4f2b5c22b2f7ba4fd119dc8
Michael Ellenburg <michael.ellenburg@mesosphere.io>
2017-09-20 15:21:19 -0700

    bar

--- dcos_launch.py
+++ dcos_launch.py
@@ -1,6 +1,21 @@
+def aws_super_duper_call():
+    raise Exception("YOU HAVENT AUTHENTICATED")
+
+
 def create(arg_list):
+    aws_super_duper_call()
     return 0
 
 
 def describe(arg_list):
     return 0
+
+
+class DcosCluster:
+    def __init__(self, config):
+        self.config
+
+
+def get_dcos_from_config(config):
+    assert isinstance(config, dict)
+    return
